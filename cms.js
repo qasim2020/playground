c30f21906bf898bc478a52bd28e609310fd6f25f
Qasim Ali <qasimali24@;2Dgmail.com>
2021-09-18 09:21:36 +0500

    minor fixes to cms.js trying to fix airtablepull

--- cms.js
+++ cms.js
@@ -3591,13 +3591,14 @@ var myFuncs = {
 
     convertStringToArticle: function( string ) {
         let body = string.split('\n').map(val => {
-            // console.log(val);
+            console.log(val);
+            if (val == '') return undefined;
             return {
               type: val.split(': ')[0].indexOf('.') != -1 ? val.split(': ')[0].split('.')[0] : val.split(': ')[0],
               msg: val.split(': ')[1].trim(),
               class: val.split(': ')[0].indexOf('.') != -1 ? val.split(': ')[0].split('.').slice(1,4).join(' ') : ''
             }
-          });
+          }).filter( val => val != undefined );
         return body;
     },
 
